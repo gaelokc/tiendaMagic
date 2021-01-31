ccfcfc64db20f9a0a481f4a08fb82c42efb28e9e
gaelokc <gael_1997@hotmail.es>
2021-01-31 19:50:53 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\login;
-use App\Models\Usuario;
+
+
+use App\Http\Controllers\cartasController;
+use App\Http\Controllers\coleccionController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ventasController;
+use App\Http\Controllers\comprasController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,3 +31,29 @@ Route::post('login', 'UserController@login');
 Route::group (['middleware' => 'auth:api'], function() {
 	Route::post('logout', 'UserController@logout');
 });
+
+Route::prefix('cartas')->group(function () {
+	Route::post('/crear',[NinjaController::class,"crearCarta"]);
+	Route::post('/editar/{id}',[NinjaController::class,"editarCarta"]);
+	Route::post('/baja/{id}',[NinjaController::class,"bajaCarta"]);
+	Route::get('/consultar/{id}',[NinjaController::class,"verCarta"]);
+	Route::get('/filtrar/{parametro}/{valor}',[NinjaController::class,"listarCartasFiltro"]);
+
+});
+Route::prefix('coleccion')->group(function () {
+	Route::post('/crear',[MisionesController::class,"crearColeccion"]);
+	Route::post('/editar/{id}',[NinjaController::class,"editarColeccion"]);
+	Route::post('/baja/{id}',[NinjaController::class,"bajaColeccion"]);
+	Route::get('/consultar/{id}',[NinjaController::class,"verColeccion"]);
+	Route::get('/filtrar/{parametro}/{valor}',[NinjaController::class,"listarColeccionesFiltro"]);
+
+});
+
+Route::prefix('ventas')->group(function () {
+		Route::post('/crear',[ClienteController::class,"crearVenta"]);
+
+	});
+
+	Route::prefix('comprar')->group(function () {
+	Route::get('/filtrar/{parametro}/{valor}',[NinjaController::class,"listarCompraFiltro"]);
+		});
